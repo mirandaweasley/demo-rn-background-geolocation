224b036e6806e1921472ed2f95ac1f616063f60d
Miranda Weasley <miranda.weasley@gmail.com>
2024-04-13 11:29:29 +0300

    Unconditional strat

--- App.tsx
+++ App.tsx
@@ -52,7 +52,7 @@ const HelloWorld = () => {
         } else {
           console.log("[authorization] SUCCESS: ", event.response);
         }
-      });      
+      });
       BackgroundGeolocation.ready({
         distanceFilter: 0,
         locationUpdateInterval: 10000,
@@ -80,13 +80,13 @@ const HelloWorld = () => {
       }).then((state) => {
         setReady(true);
         console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
-        if (!state.enabled) {
-          BackgroundGeolocation.start().then(() => {
-            console.log("- BackgroundGeolocation starting");
-            console.log("- BackgroundGeolocation.changePace(true)")
-            BackgroundGeolocation.changePace(true);
-          });
-        }
+
+        BackgroundGeolocation.start().then(() => {
+          console.log("- BackgroundGeolocation starting");
+          console.log("- BackgroundGeolocation.changePace(true)")
+          BackgroundGeolocation.changePace(true);
+        });
+
       });
     } else {
       if (ready) {
@@ -97,62 +97,62 @@ const HelloWorld = () => {
     }
   }, [enabled]);
 
-const handleLogin = async () => {
+  const handleLogin = async () => {
 
-  try {
-    const response = await fetch('https://sigcoges.com/v1/auth', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        login: username,
-        password: password,
-      }),
-    });
+    try {
+      const response = await fetch('https://sigcoges.com/v1/auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          login: username,
+          password: password,
+        }),
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    if (!response.ok) {
-      throw new Error(json.msg || 'Login failed');
+      if (!response.ok) {
+        throw new Error(json.msg || 'Login failed');
+      }
+
+      if (!json.refresh_token) {
+        throw new Error("refresh_token not found");
+      }
+
+      setRefreshToken(json.refresh_token.replace(/^Bearer /, ''));
+
+      if (!json.access_token) {
+        throw new Error("access_token not found");
+      }
+
+      setAccessToken(json.access_token.replace(/^Bearer /, ''));
+
+    } catch (error) {
+      Alert.alert("Login failed");
+
     }
+  };
 
-    if (!json.refresh_token) {
-      throw new Error("refresh_token not found");
-    }
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.label}>Username:</Text>
+      <TextInput style={styles.input} value={username} onChangeText={setUsername} placeholder="Enter your username"></TextInput>
+      <Text style={styles.label}>Password:</Text>
+      <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder="Enter your password" secureTextEntry={true}></TextInput>
+      <Button title="Login" onPress={handleLogin}></Button>
+      <Switch value={enabled} onValueChange={setEnabled} />
+      <Text style={styles.label}>Access token:</Text>
+      <Text style={styles.text}>{accessToken}</Text>
+      <Text style={styles.label}>Refresh token:</Text>
+      <Text style={styles.text}>{refreshToken}</Text>
+      <Text style={styles.text}>{location}</Text>
+      <Text style={styles.text}></Text>
+    </ScrollView>
+  )
 
-    setRefreshToken(json.refresh_token.replace(/^Bearer /, ''));
 
-    if (!json.access_token) {
-      throw new Error("access_token not found");
-    }
-
-    setAccessToken(json.access_token.replace(/^Bearer /, ''));
-
-  } catch (error) {
-    Alert.alert("Login failed");
-
-  }
-};
-
-return (
-  <ScrollView style={styles.container}>
-    <Text style={styles.label}>Username:</Text>
-    <TextInput style={styles.input} value={username} onChangeText={setUsername} placeholder="Enter your username"></TextInput>
-    <Text style={styles.label}>Password:</Text>
-    <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder="Enter your password" secureTextEntry={true}></TextInput>
-    <Button title="Login" onPress={handleLogin}></Button>
-    <Switch value={enabled} onValueChange={setEnabled} />
-    <Text style={styles.label}>Access token:</Text>
-    <Text style={styles.text}>{accessToken}</Text>
-    <Text style={styles.label}>Refresh token:</Text>
-    <Text style={styles.text}>{refreshToken}</Text>
-    <Text style={styles.text}>{location}</Text>
-    <Text style={styles.text}></Text>
-  </ScrollView>
-)
-
-  
 }
 
 export default HelloWorld;
